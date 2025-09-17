6a3e2e59ec310bbb99e9492db041e234093e1da3
dairuman <2419974912@qq.com>
2025-09-17 11:27:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-earth_weight = 50  
+weight=input ("在地球上的体重为:")
 for year in range(1, 11):  
-    earth_weight += 0.5  
+    earth_weight = 0.5*year+weight  
     moon_weight = earth_weight * 0.165  
     print("第{year}年，在地球上的体重：{earth_weight:.2f}kg，
     在月球上的体重：{moon_weight:.2f}kg")
